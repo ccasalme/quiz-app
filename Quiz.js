40482010358153c8f0d27de75cffd4fe4a1a149b
ccasalme <78170262+ccasalme@users.noreply.github.com>
2024-12-19 15:14:22 -0500

    added questions

---
+++ Quiz.js
@@ -0,0 +1,17 @@
+// Quiz.js 
+const questionsData = [
+    {
+      id: 1,
+      question: "In JJK (Jujutsu Kaisen), which character is technically Sukuna's twin and descendant? (Spoilers)",
+      options: ["Gojo", "Megumi", "Yuji", "Sukuna Himself"],
+      correctAnswer: "Yuji",
+    },
+    {
+      id: 2,
+      question: "Which character that Gege Akutami hates the most and who his wife would leave him for?",
+      options: ["Sukuna", "Toji", "Geto", "Gojo"],
+      correctAnswer: "Gojo",
+    },
+  ];
+  
+  module.exports = questionsData;
